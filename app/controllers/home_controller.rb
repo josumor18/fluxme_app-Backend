e8cca499e6fd32acfe79b46ce079b69a4a30137a
Carlos Josue Morales R <carlosjosum@hotmail.com>
2018-04-06 00:28:46 -0600

    r

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	before_action :authenticate_with_http_digest#authenticate_user!
+	before_action :authenticate_user!, raise: false
   def index
   	
   end
