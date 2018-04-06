a6ec442f569c0dee3e072110684d3b3c955f7b4c
Carlos Josue Morales R <carlosjosum@hotmail.com>
2018-04-05 23:01:47 -0600

    User authentication

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,42 @@
+module Api
+	module V1
+		class UsersController < ApplicationController
+			#acts_as_token_authentication_handler_for User
+			protect_from_forgery with: :null_session
+			def index
+				users = User.order('created_at DESC');
+				render json: { status: 'SUCCESS', message: 'Loaded users', data:users }, status: :ok
+			end
+
+			def login
+				#email = params[:email]
+				user = User.where(email: params[:email]).first
+				pass = params[:encrypted_password]#(params[:encrypted_password])
+				#token = params[:authentication_token]
+				if (user&.valid_password?(pass))# || (user.authentication_token==token)
+					user.authentication_token = nil
+					user.save
+					render json: { status: 'SUCCESS', message: 'SESION INICIADA', data:user }, status: :created
+				else
+					render json: { status: 'INVALID', message: 'Error al iniciar sesion'}, status: :unauthorized
+				end
+			end
+
+			def register
+				user = User.new(user_params)
+				if user.save
+					render json: { status: 'SUCCESS', message: 'USUARIO REGISTRADO', data:user }, status: :ok
+				elsif user = User.where(email: params[:email]).first
+					render json: { status: 'ERROR', message: 'USUARIO EXISTENTE' }, status: :unauthorized
+				else
+					render json: { status: 'ERROR', message: 'USUARIO NO CREADO' }, status: :bad
+				end
+			end
+
+			private
+			def user_params
+				params.permit(:name, :email, :password, :password_confirmation)
+			end
+		end
+	end
+end
