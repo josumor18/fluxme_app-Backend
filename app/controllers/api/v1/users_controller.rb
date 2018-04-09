e1fd24f98c1efa15d3eb8d1c65a8d08cfd9729d8
Carlos Josue Morales R <carlosjosum@hotmail.com>
2018-04-08 18:44:22 -0600

    metodos login, login_facebook y login_token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,10 +16,49 @@ module Api
 				if (user&.valid_password?(pass))# || (user.authentication_token==token)
 					user.authentication_token = nil
 					user.save
-					render json: { status: 'SUCCESS', message: 'SESION INICIADA', data:user }, status: :created
+					render json: { status: 'SUCCESS', message: 'SESION INICIADA', data:user }, status: :ok
 				else
 					render json: { status: 'INVALID', message: 'Error al iniciar sesion'}, status: :unauthorized
 				end
+			end
+
+			def login_token
+				user = User.where(email: params[:email]).first
+				token = params[:authentication_token]
+				
+				if (user.authentication_token==token)
+					user.authentication_token = nil
+					user.save
+					render json: { status: 'SUCCESS', message: 'SESION INICIADA', data:user }, status: :ok
+				else
+					render json: { status: 'INVALID', message: 'Error al iniciar sesion'}, status: :unauthorized
+				end
+			end
+
+			def login_facebook
+				#email = params[:email]
+				user = User.where(email: params[:email]).first
+				#pass = params[:encrypted_password]#(params[:encrypted_password])
+				token = params[:authentication_token]
+				if user
+					if (user.authentication_token==token)
+						user.authentication_token = nil
+						user.save
+						render json: { status: 'SUCCESS', message: 'SESION INICIADA', data:user }, status: :ok
+					else
+						render json: { status: 'INVALID', message: 'Error al iniciar sesion'}, status: :unauthorized
+					end
+				else
+					user = User.new(name: params[:name], email: params[:email], password: params[:authentication_token], password_confirmation: params[:authentication_token])
+					if user.save
+						render json: { status: 'SUCCESS', message: 'SESION INICIADA', data:user }, status: :ok
+					elsif user = User.where(email: params[:email]).first
+						render json: { status: 'ERROR', message: 'USUARIO EXISTENTE' }, status: :unauthorized
+					else
+						render json: { status: 'ERROR', message: 'USUARIO NO CREADO' }, status: :bad
+					end
+				end
+				
 			end
 
 			def register
